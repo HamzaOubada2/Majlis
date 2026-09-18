d6c73cfdf715cded10ab8f597668946ebe592f6d
HamzaOubada2 <OubHam220@gmail.com>
2026-09-19 00:19:45 +0100

    Refactor code structure for improved readability and maintainability

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,// Remove any undefined fields in the DTO
+      forbidNonWhitelisted: true,// Throw an error if the user submits unexpected fields
+      transform: true,// Automatically convert data to the specified types
+    })
+  )
+
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
