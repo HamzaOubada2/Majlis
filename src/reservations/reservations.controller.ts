57c1632792c8ef6f15219975cb673863fdd7e1f8
HamzaOubada2 <OubHam220@gmail.com>
2026-09-22 09:12:32 +0000

    Refactor code structure for improved readability and maintainability

--- src/reservations/reservations.controller.ts
+++ src/reservations/reservations.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Req, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from '../common/guards/jwt-auth.guard';
+import { ReservationsService } from './reservations.service';
+import { CreateReservationDto } from './dtos/create-reservation.dto';
 
+
+@ApiTags('Reservations')
+@ApiBearerAuth('JWT-auth')
+@UseGuards(JwtAuthGuard)
 @Controller('reservations')
-export class ReservationsController {}
+export class ReservationsController {
+    constructor(private readonly reservationsService: ReservationsService) {}
+
+    @ApiOperation({summary: 'Reserve a seat at a seminar (Token required)'})
+    @Post()
+    async create(@Req() req: any, @Body() createReservationDto: CreateReservationDto) {
+        const userId = req.user.sub;
+        return this.reservationsService.createReservation(userId, createReservationDto.seminarId)
+    }
+
+    @ApiOperation({summary: 'View current users bookings'})
+    @Get('my-reservations')
+    async getMyReservations(@Req() req: any) {
+        const userId = req.user.sub;
+        return this.reservationsService.getUserReservations(userId);
+    }
+
+
+    @ApiOperation({summary: 'Cancel previous reservation'})
+    @Delete(':id')
+    async cancel(@Req() req:any, @Param('id') id:string) {
+        const userId = req.user.sub;
+        await this.reservationsService.cancelReservation(userId, id);
+        return { message: 'تم إلغاء الحجز بنجاح وإعادة المقعد المتاح' };    
+    }
+}
