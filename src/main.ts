1a6c3b0e8f33c074c5f42fa5e1659ab651cbd263
HamzaOubada2 <OubHam220@gmail.com>
2026-09-23 09:37:06 +0000

    Fix syntax error

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const x:number = "error";
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
